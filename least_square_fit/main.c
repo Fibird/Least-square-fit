1376d9b835b42a54dcbadc35dd7ceac8e6605289
Chaoyang Liu <chaoyanglius@gmail.com>
2016-06-22 23:49:34 +0800

    finish this branch

--- least_square_fit/main.c
+++ least_square_fit/main.c
@@ -2,75 +2,96 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define N 4
+#define L -5.0
+#define R 5.0
+#define h 0.1
+
 // Weight fuction
 double omega(double x)
 {
-    return x;
+    return 1.0;
+}
+
+// Use Legendre polynomial as family of orthogonal function
+double legendre(int n, double x)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else if(n == 1)
+    {
+        return x;
+    }
+    else
+    {
+        return ((2 * n) * x * legendre(n - 1, x) - (n - 1) * legendre(n-2, x)) / n;
+    }
 }
 
 int main()
 {
-    double P = 1.0;
-    double alpha = 0.0;
-    double beta = 0.0;
-    double a = 0.0;       // Coefficients of x
+    double P;       // Values of orthogonal function
+    double denominator = 0.0;       // Must be initialized!
+    double numerator = 0.0;         // Must be initialized!
+    double *a;       // Coefficients of function P
+    double S = 0.0;     // Fitted values
+    FILE* fp = fopen("result.txt", "w+");
 
     int i = 0, j = 0;
-    int n = 0;          // Number of iterations
-    double t1 = 0.0, t2 = 0.0, t3 = 0.0;  //temporary variable
-    double prev_P = 0.0;
+    int m = 0;          // Number of data
+    double temp;  // temporary variable
+    //double t1 = 0.0, t2 = 0.0, t3 = 0.0;  //temporary variable
+    //double prev_P = 0.0;
 
     double *x, *y;      // Values of x & values of y
 
+    a = (double*) malloc(N * sizeof(double));
     printf("Please enter the number of data: ");
-    scanf("%d", &n);
+    scanf("%d", &m);
 
     // Allocates memory to array x and array y
-    x = (double*) malloc(n * sizeof(double));
-    y = (double*) malloc(n * sizeof(double));
+    x = (double*) malloc(m * sizeof(double));
+    y = (double*) malloc(m * sizeof(double));
 
     printf("Please enter values of x:\n");
     // Inputs the data given
-    for (i = 0; i < n; i++)
+    for (i = 0; i < m; i++)
     {
         scanf("%lf", &x[i]);
     }
     printf("Please enter values of y:\n");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < m; i++)
     {
         scanf("%lf", &y[i]);
     }
 
-    prev_P = P;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < N; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < m; j++)
         {
-            t1 += omega(x[j]) * x[j] * P * P;
-            t2 += omega(x[j]) * P * P;
+            P = legendre(i, x[j]);
+            denominator += omega(x[j]) * y[j] * P;
+            numerator += omega(x[j]) * P * P;
         }
-        alpha = t1 / t2;
-        if (i != 0)
-        {
-            for (j = 0; j < n; j++)
-            {
-                t3 += omega(x[j]) * prev_P * prev_P;
-            }
-            beta = t2 / t3;
-        }
-        prev_P = P;
-        // Computes the value of P,
-        // and P is a orthogonal family of function
-        P = (x[i] - alpha) * P - beta * prev_P;
-        for (j = 0; j < n; j++)
-        {
-            t1 += omega(x[j]) * P * P;
-            t2 += omega(x[j]) * y[j] * P;
-        }
-        // Computes the coefficients
-        a = t2 / t1;
-        printf("%lf ", a);
+        a[i] = numerator / denominator;
     }
+
+    for (temp = L; temp <= R; temp += h)
+    {
+        for (j = 0; j < N; j++)
+        {
+            S += a[j] * legendre(j, temp);
+        }
+        fprintf(fp, "%lf\t%lf\n", temp, S);
+    }
+
+    // Close file stream and free memory
+    fclose(fp);
+    free(x);
+    free(y);
+    free(a);
 
     return 0;
 }
